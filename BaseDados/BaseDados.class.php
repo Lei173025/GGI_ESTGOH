c768654ad52d6f3a4ae87f27d5fb86785bd8cf8c
Emanuel Rodrigues <emanuelarroja1998@gmail.com>
2018-12-14 23:54:30 +0000

    Update BaseDados.class.php

--- BaseDados/BaseDados.class.php
+++ BaseDados/BaseDados.class.php
@@ -1,1 +1,28 @@
+<?php
+class BaseDados{
 
+  private $host = "localhost";
+  private $dbname = "ggi";
+  private $user = "root";
+  private $pass = "root";
+  public $DBH=null;// database handler
+
+  public function __construct(){
+    $this->ligar_bd();
+
+  }
+  public function ligar_bd(){
+    try{
+      $this->DBH=new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->user, $this->pass);
+      $this->DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+  }
+
+  public function desligar_bd(){
+    $this->DBH=null;
+  }
+}
+
+ ?>
