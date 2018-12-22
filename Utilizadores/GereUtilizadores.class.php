96d172750110b41682dc2a1137d48d54d4f95114
Emanuel Rodrigues <emanuelarroja1998@gmail.com>
2018-12-22 14:59:49 +0000

    Update GereUtilizadores.class.php

--- Utilizadores/GereUtilizadores.class.php
+++ Utilizadores/GereUtilizadores.class.php
@@ -16,7 +16,7 @@ class GereUtilizadores{
   * Devolve o utilizador caso a autenticação seja efetuada com sucesso, caso contrário devolve null
   */
   public function autenticacao($email, $password){
-
+    
     $array_email=explode("@",$email);
     $username=$array_email[0];
 
@@ -85,48 +85,49 @@ class GereUtilizadores{
     unset($_SESSION['USER_GESTOR']);
     unset($_SESSION['USER_ESTADO']);
   }
-/*
-* Construtor da classe Gere_Utilizadores, cria um objeto Gere_Utilizadores com uma ligaçõa à base de dados local
-*
-*/
-public function __construct($DBH){
-   $this->acesso_base_dados = $DBH;
-}
-/*
-* Método que insere o objeto Utilizador recebido na base de dados local, caso tenha verificado que não existe esse utilizador na base de dados
-* Devolve 1 se inserido com Sucesso, 0 com Insucesso
-*/
-public function insere_user(Utilizador $user): int{
-  $CheckUserSTH = $this->DBH-prepare("SELECT COUNT(*) FROM USERS WHERE USER_EMAIL = ?");
-  $CheckUserSTH->bindParam(1,$user->getUser_email());
-  $CheckUserSTH->execute();
-  $CheckUserSTH->setFetchMode(PDO::FETCH_ASSOC);
-  while($row = $CheckUserSTH->fetch()){
-    if($row["COUNT(*)"] == 0){
-      $STH = $this->DBH->prepare("INSERT INTO UTILIZADOR(USER_NOME,USER_EMAIL,USER_IMAGEM,USER_TIPO,USER_GESTOR,USER_ESTADO) values(?,?,?,?,?,?)");
-      if(!$STH->execute(array($user->getUser_nome(),$user->getUser_email(),$user->getUser_imagem(),$user->getUser_tipo(),$user->getUser_gestor(),$user->getUser_estado()))){
-        return 1;
-        exit;
-      }
-    }else {
+  /*
+  * Construtor da classe Gere_Utilizadores, cria um objeto Gere_Utilizadores com uma ligaçõa à base de dados local
+  *
+  */
+  public function __construct($DBH){
+    $this->acesso_base_dados = $DBH;
+  }
+  /*
+  * Método que insere o objeto Utilizador recebido na base de dados local, caso tenha verificado que não existe esse utilizador na base de dados
+  * Devolve 1 se inserido com Sucesso, 0 com Insucesso
+  */
+  public function insere_user(Utilizador $user): int{
+    $CheckUserSTH = $this->DBH-prepare("SELECT COUNT(*) FROM USERS WHERE USER_EMAIL = ?");
+    $CheckUserSTH->bindParam(1,$user->getUser_email());
+    $CheckUserSTH->execute();
+    $CheckUserSTH->setFetchMode(PDO::FETCH_ASSOC);
+    while($row = $CheckUserSTH->fetch()){
+      if($row["COUNT(*)"] == 0){
+        $STH = $this->DBH->prepare("INSERT INTO UTILIZADOR(USER_NOME,USER_EMAIL,USER_IMAGEM,USER_TIPO,USER_GESTOR,USER_ESTADO) values(?,?,?,?,?,?)");
+        if(!$STH->execute(array($user->getUser_nome(),$user->getUser_email(),$user->getUser_imagem(),$user->getUser_tipo(),$user->getUser_gestor(),$user->getUser_estado()))){
+          return 1;
+          exit;
+        }
+      }else {
         return 0;
+      }
     }
   }
-}
-/*
-* Método que obtém a lista de todos os utilizadores ativos na base de dados local
-*
-* Devolve a lista de utilizadores ativos em forma de array
-*
-*/
-public function listar_users(){
-  $STH = $this->DBH->prepare("SELECT * FROM USERS WHERE USER_ESTADO = 'TRUE'");
-  $STH->execute();
-  $STH->setFetchMode(PDO::FETCH_ASSOC);
-  $this->utilizadores = array();
-  while($row = $STH->fetch()){
-    array_push($this->utilizadores,new Utilizador($row["USER_NOME"],$row["USER_EMAIL"],$row["USER_IMAGEM"],$row["USER_TIPO"],$row["USER_GESTOR"],$row["USER_ESTADO"]));
+  /*
+  * Método que obtém a lista de todos os utilizadores ativos na base de dados local
+  *
+  * Devolve a lista de utilizadores ativos em forma de array
+  *
+  */
+  public function listar_users(){
+    $STH = $this->DBH->prepare("SELECT * FROM USERS WHERE USER_ESTADO = 'TRUE'");
+    $STH->execute();
+    $STH->setFetchMode(PDO::FETCH_ASSOC);
+    $this->utilizadores = array();
+    while($row = $STH->fetch()){
+      array_push($this->utilizadores,new Utilizador($row["USER_NOME"],$row["USER_EMAIL"],$row["USER_IMAGEM"],$row["USER_TIPO"],$row["USER_GESTOR"],$row["USER_ESTADO"]));
 
+    }
   }
 
   public function insere_user(Utilizador $user): int{
