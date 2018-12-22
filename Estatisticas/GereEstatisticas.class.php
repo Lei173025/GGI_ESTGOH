8eac0ae8bb55954da0bb1f749bb1912dfe6ead19
RicardoMaria <ricardo.maria_97@hotmail.com>
2018-12-22 15:48:16 +0000

    Update GereEstatisticas.class.php

--- Estatisticas/GereEstatisticas.class.php
+++ Estatisticas/GereEstatisticas.class.php
@@ -1,1 +1,15 @@
+<?php
+class GereEstatisticas{
+  private $estatisticas;
+  private $acesso_base_dados;
 
+public function __construct($DBH){
+   $this->acesso_base_dados = $DBH;
+}
+
+//listar
+
+}
+
+
+?>
